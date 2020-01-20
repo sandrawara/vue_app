dffd0f5c2e436f031a47907af34f8b1545423b4b
Sandra Wara <sakr0903@student.miun.se>
2020-01-20 09:10:22 +0100

    first

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const cors = require('cors');
 
 const app = express();
 
 //Middleware
-app.use(express.json());
+app.use(bodyParser.json());
 app.use(cors());
 
 //Link to api/routes
